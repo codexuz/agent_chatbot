77b7542fe193a30456b76cb02b72f9b18de023ec
codexuz <142141576+codexuz@users.noreply.github.com>
2024-10-20 22:45:14 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,25 @@
+require('dotenv').config();
+const { webhookCallback } = require('grammy');
 
+const express = require('express');
+const authRoutes = require('../routes/authRoutes');
+const pdfRoutes = require('../routes/pdfRoutes');
+const botRoutes = require('../routes/botRoutes');
+const  bot  = require('../controllers/telegramController');
+
+
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/pdf', pdfRoutes);
+app.use('/api/bot', botRoutes); // Bot routes for saving tokens
+app.use(webhookCallback(bot, 'express'))
+
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
